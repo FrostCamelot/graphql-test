ddda30527565d3136a38585027f6928f8b760656
Шанин Артём Юрьевич <joymefistoggc@gmail.com>
2019-11-28 00:14:39 +0300

    First commit

---
+++ src/db.js
@@ -0,0 +1,9 @@
+export let users = [
+  { id: 1, name: "Ang Doe", email: "john@gmail.com", age: 22 },
+  { id: 2, name: "Jane Snow", email: "jane@gmail.com", age: 23 }
+];
+
+export let customer = [
+  { companyName: 'Azino', address: { city: 'Moscow', country: 'Russia' } },
+  { companyName: 'Альпари', address: { city: 'Альпака', country: 'Мексика' } },
+];
